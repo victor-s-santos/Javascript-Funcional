68510b0a44fb1ed6ec018a6f434042d9c8091599
victor-s-santos <victor.s.silva@unesp.br>
2019-07-29 15:41:26 -0300

    entendendo async_await

---
+++ NodeJs/módulo1/scripts_aprendizado/asyn_await.js
@@ -0,0 +1,43 @@
+//resolver as promises com async await
+
+function Usuario_assincrono2(){
+    return new Promise(function ResolvePromise(resolve, reject){
+        setTimeout(function (){
+            return resolve({
+                id: 0,
+                nome: 'Victor',
+                data: new Date()
+            })
+        }, 1000)
+
+    })
+
+}
+function obterTelefone(id){
+    return new Promise(function ResolvePromise(resolve, reject){
+        setTimeout(() => {
+            return resolve({
+                telefone: '33164035',
+                ddd:'13'
+            })
+        }, 2000);
+    })
+}
+//o fato de adicionar a palavra async faz com que o retorno seja uma promise
+
+async function main(){
+    try{
+        const usuario = await Usuario_assincrono2()
+        const telefone = await obterTelefone(usuario.id)
+        console.log(`
+        Nome: ${usuario.nome},
+        Telefone: (${telefone.ddd})${telefone.telefone}`)
+    }
+    catch(error){
+        console.error('Algo de errado não está certo!', error)
+
+    }
+}
+main()
+
+//maneira mais simples que a anterior de resolver promises
