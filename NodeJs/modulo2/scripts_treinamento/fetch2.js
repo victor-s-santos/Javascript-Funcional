b120c285876a995203c4df55bfcbec45541f8497
victor-s-santos <victor.s.silva@unesp.br>
2019-08-05 09:35:27 -0300

    finalizado modulo3

---
+++ NodeJs/modulo2/scripts_treinamento/fetch2.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+fetch('https://cotaboxvictor.herokuapp.com/api/Participation/?format=json')
+  .then((res) => { 
+    return res.json() 
+  })
+  .then((Data) => {
+    resultado = Data[0];
+    // var nome = [];
+    // var participacao = [];
+    // const w = Data.length - 1;
+    // i = 0;
+    // while(i<= w){
+    //     nome.push(Data[i].first_name);
+    //     participacao.push(Data[i].participation)
+    //     i ++;
+    
+    console.log(resultado);  
+  })
+  .catch((err) => {
+    console.error('Algum problema ocorreu:', err);
+  });
+  //como eu possuo agora um array que é o primeiro nome, e um outro que é a participação
+  //vou implementar métodos de arrays
