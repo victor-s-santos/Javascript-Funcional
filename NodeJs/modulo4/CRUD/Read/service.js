53b216e289f00b108f8cffb4dd51ffa79e1ed78c
victor-s-santos <victor.s.silva@unesp.br>
2019-08-05 12:31:09 -0300

    modulo4 CREATE

--- NodeJs/modulo4/CRUD/Read/service.js
+++ NodeJs/modulo4/CRUD/Read/service.js
@@ -1,11 +1,14 @@
 const {
-    readFile
+    readFile,
+    writeFile
 } = require('fs')
 const { 
     promisify
 } = require('util')
 
+//convertendo para promises
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 //outra forma de obter dados de um json
 // const dadosJson = require('./herois.json)
 
@@ -17,12 +20,29 @@ class Database{
         const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
         return JSON.parse(arquivo.toString())
     }
-    escreverHeroi(){
+    async escreverHeroi(dados){
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        return true
+
+    }
+    async cadastrarHeroi(heroi){
+        const dados = await this.obterHeroi()
+        const id = heroi.id <= 2 ? heroi.id : Date.now();
+        const heroicomId = {
+            id,
+            ...heroi
+        }
+        const dadosFinal = [
+            ...dados,
+            heroicomId
+        ]
+        const resultado = await this.escreverHeroi(dadosFinal)
+        return resultado
 
     }
     async listar(id){
         const dados = await this.obterHeroi()
-        const heroisFiltrados = dados.filter(item =>(id ? (item.id === id): true))//15:00
+        const heroisFiltrados = dados.filter(item =>(id ? (item.id === id): true))
         return heroisFiltrados
     }
 }
--- NodeJs/modulo4/CRUD/Read/test.js
+++ NodeJs/modulo4/CRUD/Read/test.js
@@ -11,10 +11,19 @@ const ITEM_PADRAO = {
 }
 
 describe('Suite de manipulação de Heróis!', ()=>{
+    before(async () => {
+        await database.cadastrarHeroi(ITEM_PADRAO)//evita de executar os próximos passos, sem item cadastrado
+    })
     it('deve pesquisar um herói usando arquivos', async () => {
         const expected = ITEM_PADRAO
         const [resultado] = await database.listar(expected.id)
         deepEqual(resultado, expected)
+    })
+    it('deve cadastrar um herói, usando arquivos', async () => {
+        const expected = ITEM_PADRAO
+        const resultado = await database.cadastrarHeroi(ITEM_PADRAO)
+        const [atual] = await database.listar(ITEM_PADRAO.id)
+        deepEqual(atual, expected)
     })
 
 })
