d0476ef5a731c341d89534c64c321607b373f319
victor-s-santos <victor.s.silva@unesp.br>
2019-08-22 18:09:28 -0300

    escrevendo informacoes coletadas com fetch

--- NodeJs/modulo1/scripts_aprendizado/praticando_nodejs.js
+++ NodeJs/modulo1/scripts_aprendizado/praticando_nodejs.js
@@ -3,3 +3,20 @@ const fs = require('fs');
 
 fs.writeFileSync('arquivo.txt', 'Primeiro teste: escrevendo arquivo!')
 
+//escrever de uma api
+const fetch = require('node-fetch');
+fetch('https://cotaboxvictor.herokuapp.com/api/Participation/?format=json')
+.then((res) => {  
+    return res.json() 
+  })
+  .then((Data) => {
+    console.log(`
+    Primeiro nome: ${Data[0].first_name}
+    Último nome: ${Data[0].last_name}
+    Participação: ${Data[0].participation}`);
+    fs.writeFileSync('arquivo2.txt', `Primeiro nome: ${Data[0].first_name}`)
+  })
+  .catch((err) => {
+    console.error(err);
+  }); 
+ 
