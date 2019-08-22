8c7438ac7a665edb68836dbe3640ab1bf907b758
victor-s-santos <victor.s.silva@unesp.br>
2019-08-22 18:30:24 -0300

    gerando um laco pra adicionar todos os nomes

--- NodeJs/modulo1/scripts_aprendizado/praticando_nodejs.js
+++ NodeJs/modulo1/scripts_aprendizado/praticando_nodejs.js
@@ -10,13 +10,20 @@ fetch('https://cotaboxvictor.herokuapp.com/api/Participation/?format=json')
     return res.json() 
   })
   .then((Data) => {
-    console.log(`
-    Primeiro nome: ${Data[0].first_name}
-    Último nome: ${Data[0].last_name}
-    Participação: ${Data[0].participation}`);
-    fs.writeFileSync('arquivo2.txt', `Primeiro nome: ${Data[0].first_name}`)
+    var nome = [];
+            //var participacao = [];
+            const w = Data.length - 1;
+            let i = 0;
+            while(i <= w){
+                nome.push(`${Data[i].first_name}  ${Data[i].last_name} \n`);
+                //participacao.push(`${Data[i].participation}`);
+                i ++; 
+            } 
+            console.log(nome)
+    fs.writeFileSync('arquivo2.txt',nome)
   })
   .catch((err) => {
     console.error(err);
-  }); 
+  });
+  //pegar todos os nomes, e associar as suas notas de participação e escrever em um arquivo externo
  
