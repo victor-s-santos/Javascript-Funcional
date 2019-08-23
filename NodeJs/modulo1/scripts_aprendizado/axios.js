8d9af474ebc46dcbb4b4a461b0b1c1507eeedc00
victor-s-santos <victor.s.silva@unesp.br>
2019-08-23 18:17:22 -0300

    trabalhando com axios

--- NodeJs/modulo1/scripts_aprendizado/axios.js
+++ NodeJs/modulo1/scripts_aprendizado/axios.js
@@ -1,7 +1,15 @@
 //brincando com axios, com o meu primeiro github^^
 const axios = require('axios');
+const fs = require('fs');
+let id = '';
 axios.get('https://api.github.com/users/' + 'criolodoido')
   .then(function(response){
-    console.log(response.data); 
-    console.log(response.status); 
+    if(response.status === 200){
+      fs.writeFileSync('arquivo3.txt', `Nome de usuário: ${response.data.login} \n id: ${response.data.id} \n Nome: ${response.data.name} \n Biografia: ${response.data.bio}`)
+    }else{
+      console.error(`Algo de errado não está certo! ${error}`)
+    } 
   }); 
+  //http://codeheaven.io/how-to-use-axios-as-your-http-client-pt/
+
+  //https://flaviocopes.com/node-axios/
