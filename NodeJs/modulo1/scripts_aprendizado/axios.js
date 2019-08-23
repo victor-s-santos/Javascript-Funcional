81ce43da5d66bcdd06363a640b344fc146fe840c
victor-s-santos <victor.s.silva@unesp.br>
2019-08-23 18:34:10 -0300

    acrescentando catch ao codigo

--- NodeJs/modulo1/scripts_aprendizado/axios.js
+++ NodeJs/modulo1/scripts_aprendizado/axios.js
@@ -4,12 +4,9 @@ const fs = require('fs');
 let id = '';
 axios.get('https://api.github.com/users/' + 'criolodoido')
   .then(function(response){
-    if(response.status === 200){
-      fs.writeFileSync('arquivo3.txt', `Nome de usuário: ${response.data.login} \n id: ${response.data.id} \n Nome: ${response.data.name} \n Biografia: ${response.data.bio}`)
-    }else{
-      console.error(`Algo de errado não está certo! ${error}`)
-    } 
-  }); 
-  //http://codeheaven.io/how-to-use-axios-as-your-http-client-pt/
-
-  //https://flaviocopes.com/node-axios/
+    fs.writeFileSync('arquivo3.txt', `Nome de usuário: ${response.data.login} \n id: ${response.data.id} \n Nome: ${response.data.name} \n Biografia: ${response.data.bio}`) 
+  })
+  .catch(function(error){
+    console.error('Algo de errado não está certo!', error)
+  })
+ 
