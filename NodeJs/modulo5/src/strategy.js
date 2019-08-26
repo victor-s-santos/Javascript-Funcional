1108c22e41b519b4cb75f26aabd9f7978c51821d
victor-s-santos <victor.s.silva@unesp.br>
2019-08-26 18:24:51 -0300

    criada a estrutura dos arquivos de estrategia

--- NodeJs/modulo5/src/strategy.js
+++ NodeJs/modulo5/src/strategy.js
@@ -22,3 +22,47 @@ class ICrud{
         throw new NoImplementedException()
     }
 }
+
+class MongoDB extends ICrud{
+    constructor(){
+        super()
+    }
+    create(item){
+        console.log('Salvo no banco de dados MongoDB')
+    }
+}
+class Postgres extends ICrud{
+    constructor(){
+        super()
+    }
+    create(item){
+        console.log('Salvo no banco de dados Postgres')
+    }
+}
+
+class ContextStrategy{
+    constructor(strategy){
+        this._database = strategy
+    }
+
+    create(item){
+        return this._database.create(item)
+    }
+    read(item){
+        return this._database.read(item)
+    }
+    update(id, item){
+        return this._database.update(id, item)
+    }
+    delete(id){
+        return this._database.delete(id)
+    }
+}
+
+const contextMongo = new ContextStrategy(new MongoDB())
+contextMongo.create()
+
+const contextPostgres = new ContextStrategy(new Postgres())
+contextPostgres.create()
+
+contextMongo.read()
