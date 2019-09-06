2c35d5f7c4b8349ac57bdd7434b38ec548875aed
victor-s-santos <victor.s.silva@unesp.br>
2019-09-06 11:48:41 -0300

    metodo map e metodo reduce

--- Pré-NodeJs/Objetos/script2.js
+++ Pré-NodeJs/Objetos/script2.js
@@ -17,6 +17,22 @@ const lista = ['Futebol', 'Capoeira', 'Surf', pessoa]
 //armazenando um objeto(pessoa), dentro de um array, e chamando um método(boasvindas)
 lista[3].boasvindas()
 
-//similar ao range do python
-const numbers = Array.from({length: 50}, (x,i) => i);
-numbers
+const esportes = ['Capoeira', 'Futebol', 'Programar', 'Correr', 'Lutar']
+//map sempre recebe uma função onde onde é definido como adicionar 
+//ou editar os elementos dentro deste array
+// console.log(esportes.map(esporte => {
+//     return `Esporte ${esporte}`;
+// }));
+
+//em uma linha
+console.log(esportes.map(esporte => `Esporte: ${esporte}`));
+//mas sem mudar o array. Logom o uso do map retorna um novo array
+console.log(esportes)
+
+
+const numeros = [-1, 2, -3, 4, -5, 6];
+const soma = (acumulado, valoratual) => acumulado + valoratual;
+console.log(`Usando o método reduce ${numeros.reduce(soma)}`);
+//o reduce aplica a fc soma sobre todos os elementos da lista, onde acumulado é 
+//numeros[i] e valoratual é numeros[i+1]
+//-1+2-3+4-5+6 = 3
